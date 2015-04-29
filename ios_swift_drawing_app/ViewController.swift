e05d313481bd62e71c6fda6782bd4581e41bdc5f
Maxim Bilan <maximb.mail@gmail.com>
2015-04-29 10:14:22 +0300

    Added project template

---
+++ ios_swift_drawing_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_swift_drawing_app
+//
+//  Created by Maxim Bilan on 4/29/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
