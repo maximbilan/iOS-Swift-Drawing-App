bb5351cd66a2a04bfe276b25052b094a4ebe921e
Maxim Bilan <maximb.mail@gmail.com>
2015-04-29 10:30:13 +0300

    Added drawing view class

---
+++ ios_swift_drawing_app/DrawingView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawingView.swift
+//  ios_swift_drawing_app
+//
+//  Created by Maxim Bilan on 4/29/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class DrawingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
