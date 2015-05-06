9397aa0df14a0bef885b8490fd3a73e238ac70df
Maxim Bilan <maximb.mail@gmail.com>
2015-05-06 10:10:10 +0300

    Modified drawing view

--- ios_swift_drawing_app/DrawingView.swift
+++ ios_swift_drawing_app/DrawingView.swift
@@ -22,12 +22,41 @@ class DrawingLine {
 
 class DrawingView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+	var lines: [DrawingLine] = []
+	var lastPoint: CGPoint!
+	var drawColor = UIColor.blackColor()
+	var lineWidth: CGFloat = 5
+	
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+	
+	override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+		let touch: AnyObject? = touches.first
+		lastPoint = touch!.locationInView(self)
+	}
+	
+	override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+		let touch: AnyObject? = touches.first
+		
+		var newPoint = touch!.locationInView(self)
+		lines.append(DrawingLine(start: lastPoint, end: newPoint, color: drawColor))
+		lastPoint = newPoint
+		
+		self.setNeedsDisplay()
+	}
+	
+	override func drawRect(rect: CGRect) {
+		var context = UIGraphicsGetCurrentContext()
+		CGContextBeginPath(context)
+		CGContextSetLineWidth(context, lineWidth)
+		CGContextSetLineCap(context, kCGLineCapRound)
+		for line in lines {
+			CGContextMoveToPoint(context, line.start.x, line.start.y)
+			CGContextAddLineToPoint(context, line.end.x, line.end.y)
+			CGContextSetStrokeColorWithColor(context, line.color.CGColor)
+			CGContextStrokePath(context)
+		}
+	}
 
 }
